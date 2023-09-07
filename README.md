d2a47c997f8a77666daef283e8f110648e621322
vybor <vyborov_95@mail.ru>
2023-09-07 16:27:17 +0300

    New file

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # AQA
+## Как запустить:
+   Создайте venv и установите требования
+   
+   cd (путь к папки с проектом)
+       
+       Mac OS
+          python3 -m venv env
+          source env/bin/activate - for Unix or MacOS
+          pip install -r requirements.txt
+  
+        Windows
+          python -m venv venv
+          venv\Scripts\activate
+          cd "нужная дериктория"
+          pip install -r requirements.txt 
---
+++ python_qa_selenoid/conftest.py
@@ -0,0 +1,111 @@
+import os
+import random
+import time
+
+import allure
+import pytest
+import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome")
+    parser.addoption("--executor", action="store", default="localhost")
+    parser.addoption("--mobile", action="store_true")
+    parser.addoption("--vnc", action="store_true")
+    parser.addoption("--logs", action="store_true")
+    parser.addoption("--video", action="store_true")
+    parser.addoption("--bv")
+
+
+@allure.step("Waiting for availability {url}")
+def wait_url_data(url, timeout=10):
+    """Метод ожидания доступности урла"""
+    while timeout:
+        response = requests.get(url)
+        if not response.ok:
+            time.sleep(1)
+            timeout -= 1
+        else:
+            if "video" in url:
+                return response.content
+            else:
+                return response.text
+    return None
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+# https://github.com/pytest-dev/pytest/issues/230#issuecomment-402580536
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+    if rep.outcome != "passed":
+        item.status = "failed"
+    else:
+        item.status = "passed"
+
+
+@pytest.fixture
+def browser(request):
+    browser = request.config.getoption("--browser")
+    executor = request.config.getoption("--executor")
+    vnc = request.config.getoption("--vnc")
+    version = request.config.getoption("--bv")
+    logs = request.config.getoption("--logs")
+    video = request.config.getoption("--video")
+    mobile = request.config.getoption("--mobile")
+
+    executor_url = f"http://{executor}:4444/wd/hub"
+
+    if browser == "chrome":
+        options = ChromeOptions()
+    elif browser == "firefox":
+        options = FirefoxOptions()
+
+    caps = {
+        "browserName": browser,
+        "browserVersion": version,
+        "selenoid:options": {
+            "enableVNC": vnc,
+            "name": os.getenv("BUILD_NUMBER", str(random.randint(9000, 10000))),
+            "screenResolution": "1280x2000",
+            "enableVideo": video,
+            "enableLog": logs,
+            "timeZone": "Europe/Moscow",
+            "env": ["LANG=ru_RU.UTF-8", "LANGUAGE=ru:en", "LC_ALL=ru_RU.UTF-8"]
+
+        },
+        "acceptInsecureCerts": True,
+    }
+
+    for k, v in caps.items():
+        options.set_capability(k, v)
+
+    driver = webdriver.Remote(
+        command_executor=executor_url,
+        options=options
+    )
+
+    if not mobile:
+        driver.maximize_window()
+
+    def finalizer():
+        video_url = f"http://{executor}:8080/video/{driver.session_id}.mp4"
+
+        if request.node.status == "failed":
+            if video:
+                allure.attach(
+                    body=wait_url_data(video_url),
+                    name="video_for_" + driver.session_id,
+                    attachment_type=allure.attachment_type.MP4,
+                )
+
+        if video and wait_url_data(video_url):
+            requests.delete(url=video_url)
+
+        driver.quit()
+
+    request.addfinalizer(finalizer)
+    return driver
